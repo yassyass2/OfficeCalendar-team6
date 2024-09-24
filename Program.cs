26e1fbdb6e96ddd1e0509d54ffe0db1f601071a6
yassyass2 <yassinabde@outlook.com>
2024-09-24 22:20:49 +0200

    created webapi proj + readme
    updated readme file with description and created dotnet webapi

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # OfficeCalendar
+What is the purpose of this project? 
+
+The goal of this project is to create a ‘social agenda’ application that will make it clear to Calendify when employees will come to work and show up to certain events like cooking nights or getaways. This app should also encourage employees to show up to work physically as much as possible. 
+
+What problem does this project solve? 
+
+The website makes it easier for employees to see who will come to the office/events. It will also be easier for managers to announce and manage attendance to events 
