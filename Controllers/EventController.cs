15de94949520f45829ec5c123ad5fe7c729b8449
yassyass2 <yassinabde@outlook.com>
2024-09-28 23:50:56 +0200

    added post endpoint for Events
    can now add event through POST request (soon only with admin session)

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -18,5 +18,20 @@ namespace Controllers
             var events = await JsonEventStorage.ReadEvents();
             return Ok(events);
         }
+
+        [HttpPost()]
+        public async Task<IActionResult> CreateEvent([FromBody] Event e)
+        {
+            //nog bezig met dit
+            /*
+            if (!HttpContext.Session.GetString("IsAdmin", out var isAdmin) || isAdmin != "true")
+            {
+                return Unauthorized("Admin session is required.");
+            }
+            */
+
+            await JsonEventStorage.CreateEvent(e);
+            return Ok("Event has been created");
+        }
     }
 }
--- Services/IEventStorage.cs
+++ Services/IEventStorage.cs
@@ -12,10 +12,23 @@ namespace Services
         public string End_time { get; set; }
         public string Location { get; set; }
         public bool Admin_approval { get; set; }
+
+        public Event(string title, string description, string date, string start_time, string end_time, string location)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Description = description;
+            Date = date;
+            Start_time = start_time;
+            End_time = end_time;
+            Location = location;
+            Admin_approval = false;
+        }
     }
     public interface IEventStorage
     {
         Task<List<Event>> ReadEvents();
+        Task CreateEvent(Event e);
     }
 
     public class JsonEventStorage : IEventStorage
@@ -29,6 +42,18 @@ namespace Services
                 events = JsonSerializer.Deserialize<List<Event>>(await System.IO.File.ReadAllTextAsync(path));
             }
             return events;
+        }
+
+        public async Task CreateEvent(Event e)
+        {
+            string path = $"Data/events.json";
+            List<Event> events = new List<Event>();
+            if (File.Exists(path))
+            {
+                events = JsonSerializer.Deserialize<List<Event>>(await System.IO.File.ReadAllTextAsync(path));
+            }
+            events.Add(e);
+            await File.WriteAllTextAsync(path, JsonSerializer.Serialize(events));
         }
     }
 }
