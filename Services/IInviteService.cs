c4da81f10d856406f5c45d68f0392d6435f4ef33
yassyass2 <yassinabde@outlook.com>
2024-10-18 22:36:24 +0200

    syntax errors fix

--- Services/IInviteService.cs
+++ Services/IInviteService.cs
@@ -15,18 +15,18 @@ namespace Services
 
         public async Task<bool> SendInvite(EventAttendance invite)
         {
-            if  await _context.Attendances.Where(a => a.EventId == invite.EventId && a.UserId == invite.UserId)
+            if  (await _context.Attendances.Where(a => a.EventId == invite.EventId && a.UserId == invite.UserId))
             {
                 // employee already attending
                 return false;
             }
-            var user_to_inv = await _context.Users.Where(u => u.Id == invite.UserId)
+            var user_to_inv = await _context.Users.Where(u => u.Id == invite.UserId);
             if (user_to_inv is null)
             {
                 // employee doesn't exist
                 return false;
             }
-            var to_mail = user_to_inv
+            var to_mail = user_to_inv;
             // targeted employee not attending yet and exists, send invite via mail HERE\/\/\/
             return true;
         }
