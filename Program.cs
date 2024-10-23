05d8e6764c73cd794e6beaf8820f5f216dc78210
Maurice <mauriceboendermaker@gmail.com>
2024-10-23 15:23:05 +0200

    Added IAttendanceService as AddScoped in Program

--- Program.cs
+++ Program.cs
@@ -79,6 +79,8 @@ builder.Services.AddScoped<IEventStorage, DbEventStorage>();
 builder.Services.AddScoped<IAdminService, DbAdminService>();
 builder.Services.AddScoped<IUserService, DbUserService>();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IAttendanceService, AttendanceService>();
+
 // Email service
 builder.Services.AddScoped<IEmailService, EmailService>();
 
