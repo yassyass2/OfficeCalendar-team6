ff9f9d552144523823fc6885f89d5cec2b33115e
yassyass2 <yassinabde@outlook.com>
2024-11-05 21:42:13 +0100

    notification feature werkt

--- Filters/NotificationFilter.cs
+++ Filters/NotificationFilter.cs
@@ -5,18 +5,28 @@ using Services;
 namespace Filters{
     public class NotificationFilter : Attribute, IAsyncActionFilter
     {
-        private readonly IAttendanceService _att;
+        private readonly IUserService _user;
         private readonly IEmailService _mail;
+        private readonly IAttendanceService _att;
 
-        public NotificationFilter(IAttendanceService att, IEmailService mail)
+        public NotificationFilter(IUserService user, IEmailService mail, IAttendanceService attendanceService)
         {
-            _att = att;
+            _user = user;
             _mail = mail;
+            _att = attendanceService;
         }
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             if (context.HttpContext.Request.Method == "PUT"){
                 Console.WriteLine("Attendees are being notified of update.");
+                string lastSegment = context.HttpContext.Request.Path.ToString().TrimEnd('/').Split('/').Last();
+                var attendees = await _att.GetAttending(Guid.Parse(lastSegment));
+                attendees = attendees.ToList();
+
+                for (int i = 0; i < attendees.Count(); i++){
+                    var mail = await _user.GetEmail(attendees.ElementAt(i));
+                    await _mail.SendEmail(mail, $"event with ID {lastSegment} has changed, log into your account to see changes!");
+                }
             }
             await next();
         }
