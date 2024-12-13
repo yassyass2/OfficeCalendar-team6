addd364fef6483a74b12a080f847cb78b952cd3a
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2024-12-13 18:20:17 +0100

    (Fix) Fixed padding issue on breadcrumbs container

--- react-frontend/src/components/misc/Breadcrumbs.tsx
+++ react-frontend/src/components/misc/Breadcrumbs.tsx
@@ -14,7 +14,7 @@ const Breadcrumbs: React.FC = () => {
     return (
         <>
             <div className="row">
-                <div className="col g-0 breadcrumbs-container">
+                <div className="col breadcrumbs-container">
                     <span><a href="/" className="btn-simple">Home</a> <i className="fa-solid fa-chevron-right"></i> <a href={location.pathname} className="btn-simple">{breadcrumb}</a></span>
                 </div>
             </div>
