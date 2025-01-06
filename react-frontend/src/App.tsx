9de2cd74b37a3a7e53ac2aff2480bf7a2de33397
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-01-06 17:34:37 +0100

    Moved navbar and footer to its own respective files

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+// Import components
 import Login from './components/Login';
-import Register from './components/Register'; // Import your new Register page
+import Register from './components/Register';
 import Calendar from './components/Calendar';
 import AdminMenu from './components/AdminMenu';
 import UserDashboard from './components/UserDashboard';
 import SidePanel from './components/SidePanel';
-import Breadcrumbs from './components/misc/Breadcrumbs';
 
-// Import images
-import profileImage from './assets/profile-image-placeholder@4x.png';
+// Misc imports
+import { Breadcrumbs } from './components/misc/Breadcrumbs';
+import { Nav } from './components/misc/Nav';
+import { Footer } from './components/misc/Footer';
 
 // Import main stylesheet
 import './scss/style.scss';
@@ -17,36 +20,12 @@ import './scss/style.scss';
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="container-fluid navbar-parent">
-        <nav className="container navbar">
-          <div className="row">
-            {/* Logo container */}
-            <div className="col-auto logo-container">
-              <a href="/">
-                Office<span>Calendar<b>.</b></span>
-              </a>
-            </div>
+      {/* Navbar component */}
+      {<Nav />}
 
-            {/* Navigation container */}
-            <div className="col-auto nav-container position-absolute top-50 start-50 translate-middle">
-              <nav>
-                <a href="/">Home</a>
-                <a href="/about">About</a>
-                <a href="/contact">Contact</a>
-              </nav>
-            </div>
-
-            {/* Login status container */}
-            <div className="col-auto login-status-container">
-              <span className="login-status-text"><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Please log in to continue</a></span>
-              <img src={profileImage} alt='' width={40} height={40} />
-            </div>
-          </div>
-        </nav>
-      </div>
-
-      {/* Main Content */}
+      {/* Main content */}
       <main className="container main-content g-0 flex-1">
+        {/* Breadcrumbs component */}
         {<Breadcrumbs />}
         <Routes>
           <Route path="/" element={<Login />} />
@@ -60,17 +39,8 @@ const App: React.FC = () => {
         </Routes>
       </main>
 
-      {/* Footer */}
-      <div className="container-fluid footer-parent">
-        <footer className="container d-flex justify-content-center">
-          <div className="row">
-            <div className="col-auto text-center">
-              <p>&copy; 2024 - OfficeCalendar.</p>
-              <p>Made by Yassine, Maurice, Mohammad, Halit en Thijs</p>
-            </div>
-          </div>
-        </footer>
-      </div>
+      {/* Footer component */}
+      {<Footer />}
     </Router>
   );
 };
