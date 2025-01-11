20d5cdcb14eec2ff456a87760f050b7d488d0a1f
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-01-11 12:40:52 +0100

    Removed hardcoded mock eventdata

--- react-frontend/src/components/SidePanel.tsx
+++ react-frontend/src/components/SidePanel.tsx
@@ -1,13 +1,5 @@
 import React from 'react';
 
-const mockEventData = {
-    title: 'Weekly stand-up van Jeanine en Alex - BaseCamp',
-    date: '1 december 2014',
-    time: '09:00 - 09:30',
-    attendees: 12,
-    description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae dui id turpis venenatis ultrices vel sed urna. Nunc bibendum sodales augue sed porttitor.Suspendisse potenti. Ut ac leo magna. Phasellus rhoncus arcu vitae sem ullamcorper, in feugiat dolor pulvinar.Sed auctor leo ut lorem sollicitudin congue. Integer rhoncus, est quis scelerisque commodo, massa velit hendrerit ligula, sed posuere mi felis quis enim.Nullam efficitur diam id velit pellentesque, quis volutpat arcu faucibus. Maecenas facilisis tempor dignissim. Phasellus consectetur congue porttitor. Mauris id sem et orci ornare efficitur eget vitae tellus. Phasellus porttitor ac turpis nec feugiat. Quisque ultricies nibh id tristique feugiat.Duis dignissim eros vel felis scelerisque, id fermentum ex facilisis. Suspendisse id dolor neque. Sed pellentesque finibus urna non interdum.',
-};
 interface EventData {
     id: string;
     title: string;
