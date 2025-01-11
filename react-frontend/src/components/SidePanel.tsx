fa68c7c7b13f71d926aa7350849b434500415593
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-01-11 12:41:08 +0100

    Sidepanel now dynamically updates for a specific event

--- react-frontend/src/components/SidePanel.tsx
+++ react-frontend/src/components/SidePanel.tsx
@@ -11,47 +11,62 @@ interface EventData {
 }
 
 type Props = {
+    selectedEvent: EventData | null;
     openInviteModal: () => void;
 };
 
-const SidePanel: React.FC<Props> = ({ openInviteModal }) => {
+const SidePanel: React.FC<Props> = ({ selectedEvent, openInviteModal }) => {
+    if (!selectedEvent) {
+        return (
+            <div className="sidepanel-container">
+                <p>No event selected. Click an event to view details.</p>
+            </div>
+        );
+    }
+
     return (
         <div className="row g-0">
             <div className="col-auto sidepanel-container">
-                <h2>{mockEventData.title}</h2>
-                <div className="event-metadata">
-                    <i className="fa-regular fa-calendar"></i>
-                    <span>{mockEventData.date}</span>
+                <div className="event-container">
+                    <h2>{selectedEvent.title}</h2>
+                    <div className="event-metadata">
+                        <i className="fa-regular fa-calendar"></i>
+                        <span>{selectedEvent.date}</span>
 
-                    <i className="fa-regular fa-clock"></i>
-                    <span>{mockEventData.time}</span>
+                        <i className="fa-regular fa-clock"></i>
+                        <span>
+                            {selectedEvent.start_time} - {selectedEvent.end_time}
+                        </span>
 
-                    <i className="fa-solid fa-user"></i>
-                    <span>
-                        <span className="attendees-number">
-                            {mockEventData.attendees}
-                        </span>{' '}
-                        attending
-                    </span>
-                </div>
+                        <i className="fa-solid fa-user"></i>
+                        <span>
+                            <span className="attendees-number">{0}</span> {/* Placeholder */}
+                            attending
+                        </span>
+                    </div>
 
-                <div className="event-body">
-                    {mockEventData.description}
-                </div>
+                    <div className="event-body">{selectedEvent.description}</div>
 
-                <div className="button-group">
-                    <button className="btn-primary">
-                        ATTEND EVENT
-                        <i className="fa-solid fa-caret-right"></i>
-                    </button>
-                    <button onClick={openInviteModal} className="btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        INVITE EMPLOYEE
-                        <i className="fa-solid fa-caret-right"></i>
-                    </button>
+                    <div className="button-group">
+                        <button className="btn-primary">
+                            ATTEND EVENT
+                            <i className="fa-solid fa-caret-right"></i>
+                        </button>
+                        <button
+                            onClick={openInviteModal}
+                            className="btn-primary"
+                            type="button"
+                            data-bs-toggle="modal"
+                            data-bs-target="#exampleModal"
+                        >
+                            INVITE EMPLOYEE
+                            <i className="fa-solid fa-caret-right"></i>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default SidePanel;
