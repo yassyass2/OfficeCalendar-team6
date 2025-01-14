e3d43ee5235f81165a1d0da5f6afc8cc28a6bdfb
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-01-14 20:03:34 +0100

    Minor changes

--- react-frontend/src/components/InviteModal.tsx
+++ react-frontend/src/components/InviteModal.tsx
@@ -56,7 +56,7 @@ const InviteModal: React.FC<ModalProps> = ({ Event, onClose }) => {
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="inviteModalLabel">Notify a Coworker!</h1>
+                            <h1 className="modal-title fs-5" id="inviteModalLabel">Notify a Co-Worker!</h1>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={onClose}></button>
                         </div>
                         <div className="modal-body">
